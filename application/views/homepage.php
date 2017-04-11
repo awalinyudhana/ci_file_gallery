49c2308646224e3a5f6fae8b8806c75ca6b2989f
Hasyim <lordhasyim@gmail.com>
2017-04-12 01:53:11 +0700

    fixing pagination, item di home, kurang ukuran search bar sama tambah field category

--- application/views/homepage.php
+++ application/views/homepage.php
@@ -10,13 +10,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <link rel="stylesheet" href="<?php echo base_url("assets/custom-style.css") ?>">
     <link rel="stylesheet" href="<?php echo base_url("assets/admin/font-awesome/css/font-awesome.css"); ?>"/>
 
-    <style>
-
-
-
-
-
-    </style>
 </head>
 <body>
 <?php $this->load->view('top_menu'); ?>
@@ -25,57 +18,63 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <div class="row">
         <div class="container">
             <div class="gallery col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                <h1 class="gallery-title text-center">Gallery</h1>
+                <img src="<?php echo base_url('assets/img/header_logo.png'); ?>" class="img-responsive center-block" alt="logo">
+                <h1 class="gallery-title text-center">Cari dan Simpan dengan mudah</h1>
+                <h3 class="text-center">Unggah, temukan dan simpan file tanpa halangan</h3>
             </div>
             <div class="col-sm-6 col-sm-offset-3 gallery_product">
-                    <form action="<?php echo current_url(); ?>" method="get">
-                        <div class="input-group stylish-input-group">
-                            <input type="text" name="search" class="form-control"  placeholder="Search" >
-                            <span class="input-group-addon">
+                <form action="<?php echo current_url(); ?>" method="get">
+                    <div class="input-group stylish-input-group">
+                        <input type="text" name="search" class="form-control" placeholder="Search">
+                        <span class="input-group-addon">
                                 <button type="submit">
                                     <span class="glyphicon glyphicon-search"></span>
                                 </button>
                             </span>
-                        </div>
-                    </form>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
 </div>
 <div class="row">
     <div class="container">
-        <?php foreach($items as $item): ?>
-        <div class="col-md-12">
-            <div class="col-md-6">
-                <h4>
-                    <strong><?php echo $item['title'] . ' ('. $item['total_file'] . ') '; ?></strong>
-                </h4>
-            </div>
-            <div class="col-md-6">
-                <p class="pull-right">
-                    <a href="<?php echo site_url('home/by_category/'.$item['id_category']) ?>" class="btn btn-primary btn-outline">Lihat Semua</a>
-                </p>
-            </div>
-        </div>
-        <div class="col-md-12">
-            <?php foreach ($item['file'] as $file_item) : ?>
-            <div class="file-item">
-                <div class="gallery_product col-lg-3 col-md-3 col-sm-3 col-xs-3">
-                    <div class="thumbnail">
-                        <img src="<?php echo base_url('assets/uploads/files/') .  $file_item['thumbnail']; ?>"
-                             class="img-responsive" alt="<?php echo $file_item['title'] ?>">
-                        <a href="<?php echo site_url('home/detail/'.$file_item['id']) ?>">
-                            <h4 class="text-center"><?php echo $file_item['title'] ?></h4>
-                        </a>
-                        <p><?php echo $file_item['description'] ?></p>
-                        <div class="pull-right">
-                            <input type="button" class="btn btn-primary btn-outline" value="Download">
-                        </div>
-                    </div>
+        <?php foreach ($items as $item): ?>
+            <div class="col-md-12">
+                <div class="col-md-6">
+                    <h4>
+                        <strong><?php echo $item['title'] . ' (' . $item['total_file'] . ') '; ?></strong>
+                    </h4>
+                </div>
+                <div class="col-md-6">
+                    <p class="pull-right">
+                        <a href="<?php echo site_url('home/by_category/' . $item['id_category']) ?>"
+                           class="btn btn-primary btn-outline">Lihat Semua</a>
+                    </p>
                 </div>
             </div>
-            <?php endforeach; ?>
-        </div>
+            <div class="col-md-12">
+                <?php foreach ($item['file'] as $file_item) : ?>
+                    <div class="file-item">
+                        <div class="gallery_product col-lg-2 col-md-2 col-sm-2 col-xs-2">
+                            <div class="thumbnail">
+                                <img src="<?php echo base_url('assets/uploads/files/') . $file_item['thumbnail']; ?>"
+                                     class="img-responsive" style="" alt="<?php echo $file_item['title'] ?>">
+                                <a href="<?php echo site_url('home/detail/' . $file_item['id']) ?>">
+                                    <h4 class="text-center"><?php echo $file_item['title'] ?></h4>
+                                </a>
+                                <p><?php echo $file_item['description'] ?></p>
+                                <div class="text-center">
+                                    <span class="glyphicon glyphicon-user"></span>
+                                    <?php echo $file_item['author'] ?>
+
+                                </div>
+                                &nbsp;
+                            </div>
+                        </div>
+                    </div>
+                <?php endforeach; ?>
+            </div>
         <?php endforeach; ?>
     </div>
 </div>
